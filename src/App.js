9fe7fcc111f18a85f5e3969ba75e664f307ed966
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-28 19:54:22 +0300

    routes the different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {Switch, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
+import './App.css'
 
-import {Navbar } from './components'
+import {Navbar, Exchanges, Home, CryptoDetails,Cryptocurrencies, News } from './components'
 function App() {
   return (
     <div className="app">
@@ -9,7 +10,17 @@ function App() {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/exchanges' component={Exchanges} />
+              <Route exact path='/cryptocurrencies' component={Cryptocurrencies} />
+              <Route exact path='/crypto/:coinId' component={CryptoDetails} />
+              <Route exact path='/news' component={News} />
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
