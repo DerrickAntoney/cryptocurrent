670d47489620e7048448d56ec8c4b96bc5b34cc8
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-28 21:35:12 +0300

    Initiates redux

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         <Layout>
           <div className='routes'>
             <Routes>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/exchanges' component={Exchanges} />
-              <Route exact path='/cryptocurrencies' component={Cryptocurrencies} />
-              <Route exact path='/crypto/:coinId' component={CryptoDetails} />
-              <Route exact path='/news' component={News} />
+              <Route exact path='/' element={<Home/>} />
+              <Route exact path='/exchanges' element={Exchanges} />
+              <Route exact path='/cryptocurrencies' element={Cryptocurrencies} />
+              <Route exact path='/crypto/:coinId' element={CryptoDetails} />
+              <Route exact path='/news' element={News} />
             </Routes>
           </div>
         </Layout>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,20 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
+const {Title} = Typography;
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title = 'Total Cryptocurrencies' value='5'/></Col>
+        <Col span={12}><Statistic title = 'Total Exchanges' value='5'/></Col>
+        <Col span={12}><Statistic title = 'Total Market Cap' value='5'/></Col>
+        <Col span={12}><Statistic title = 'Total 24h Volume' value='5'/></Col>
+        <Col span={12}><Statistic title = 'Total Markets' value='5'/></Col>
+      </Row>
+    </>
   )
 }
 
