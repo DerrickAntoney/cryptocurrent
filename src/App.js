464f13d710018e717d4c1c4961323c909f322cc2
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-28 20:41:01 +0300

    Impelements footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Switch, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 import './App.css'
 
@@ -12,18 +12,27 @@ function App() {
       <div className='main'>
         <Layout>
           <div className='routes'>
-            <Switch>
+            <Routes>
               <Route exact path='/' component={Home} />
               <Route exact path='/exchanges' component={Exchanges} />
               <Route exact path='/cryptocurrencies' component={Cryptocurrencies} />
               <Route exact path='/crypto/:coinId' component={CryptoDetails} />
               <Route exact path='/news' component={News} />
-            </Switch>
+            </Routes>
           </div>
         </Layout>
-      </div>
       <div className='footer'>
-
+        <Typography.Title level={5} style={{textAlign: 'center', color:'white'}}>
+          Cryptocurrent <br/>
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+          <Link to='/news'>News</Link>
+        </Space>
+      </div>
       </div>
     </div>
   );
