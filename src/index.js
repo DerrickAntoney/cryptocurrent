9504e46108504322aa6f0ebb51c15ade8cc902b6
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-04-30 04:10:40 +0300

    Implements smooth scroll

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './app/store';
+import SmoothScrolling from './components/SmoothScroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <App />
+        <SmoothScrolling>
+          <App />
+        </SmoothScrolling>
       </Provider>
     </Router>
   </React.StrictMode>
